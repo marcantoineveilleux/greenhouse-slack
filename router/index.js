ce525c4748fd8cdb49f41cd8a460aeeb710e4880
Marc-Antoine Veilleux <maveilleux@coveo.com>
2019-06-20 15:02:24 -0400

    New samples and endpoints

--- router/index.js
+++ router/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 module.exports = function(app) {
-  app.use('/new-design-applicant', require('./routes/new_design_applicant'));
   app.use('/status-design-applicant', require('./routes/candidate_stage_change'));
+  app.use('/candidate_stage_change', require('./routes/candidate_stage_change'));  
+  app.use('/new_candidate_application', require('./routes/new_candidate_application'));  
+  app.use('/application_updated', require('./routes/application_updated'));  
 };
